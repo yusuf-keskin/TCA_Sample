a86a19e72b79f28a15c6bef5ffddc583ae4e698d
Yusuf Keskin <yusuf_keskin@outlook.com>
2023-08-15 20:41:04 +0300

    Empty Project  Start

---
+++ TCA Sample Basic App/TCA_Sample_Basic_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Sample_Basic_AppApp.swift
+//  TCA Sample Basic App
+//
+//  Created by YUSUF KESKİN on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Sample_Basic_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
